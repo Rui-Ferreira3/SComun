0ebafc46bd09b40009fcb7069051387e4c1f76da
Rui-Ferreira3 <rui.ferreira@tutanota.com>
2023-05-15 11:39:55 +0100

    created functions_rui

---
+++ functions_rui.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <vector>
+#include <math.h>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <random>
+#include <algorithm>
+
+using namespace std;
+
+vector <float> operator-(const vector <float>& m1, const vector <float>& m2){
+    
+    /*  Returns the difference between two vectors.
+     Inputs:
+     m1: vector
+     m2: vector
+     Output: vector, m1 - m2, difference between two vectors m1 and m2.
+     */
+    
+    const unsigned long VECTOR_SIZE = m1.size();
+    vector <float> difference (VECTOR_SIZE);
+    
+    for (unsigned i = 0; i != VECTOR_SIZE; ++i){
+        difference[i] = m1[i] - m2[i];
+    };
+    
+    return difference;
+}
+
+vector <float> operator/(const vector <float>& m2, const float m1){
+    
+    /*  Returns the product of a float and a vectors (elementwise multiplication).
+     Inputs:
+     m1: float
+     m2: vector
+     Output: vector, m1 * m2, product of two vectors m1 and m2
+     */
+    
+    const unsigned long VECTOR_SIZE = m2.size();
+    vector <float> product (VECTOR_SIZE);
+    
+    for (unsigned i = 0; i != VECTOR_SIZE; ++i){
+        product[i] = m2[i] / m1;
+    };
+    
+    return product;
+}
+
+vector <float> transpose (float *m, unsigned int C, unsigned int R) {
+    
+    /*  Returns a transpose matrix of input matrix.
+     Inputs:
+     m: vector, input matrix
+     C: int, number of columns in the input matrix
+     R: int, number of rows in the input matrix
+     Output: vector, transpose matrix mT of input matrix m
+     */
+    
+    vector <float> mT (C*R);
+    
+    for(unsigned n = 0; n != C*R; n++) {
+        unsigned i = n/C;
+        unsigned j = n%C;
+        mT[n] = m[R*j + i];
+    }
+    
+    return mT;
+}
+
+int main(){
+
+}
